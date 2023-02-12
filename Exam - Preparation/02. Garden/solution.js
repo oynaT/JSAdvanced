3feaea21369f857fdea7e8bc746cd61c67c20295
oynaT <chantadzhiev@gmail.com>
2023-02-12 16:39:44 +0200

    Update

--- Exam - Preparation/02. Garden/solution.js
+++ Exam - Preparation/02. Garden/solution.js
@@ -11,7 +11,12 @@ class Garden {
     }
 
     this.spaceAvailable -= spaceRequired;
-    this.plants.push({ plantName, spaceRequired, ripe: false, quantity: 0 });
+    this.plants.push({
+      plantName,
+      spaceRequired,
+      ripe: false,
+      quantity: 0
+    });
     return `The ${plantName} has been successfully planted in the garden.`;
   }
 
@@ -41,31 +46,49 @@ class Garden {
   }
 
   harvestPlant(plantName) {
+    //If the plant is not found, throw an Error:
     let plant = this.plants.find((x) => x.plantName == plantName);
-
     if (!plant) {
       throw new Error(`There is no ${plantName} in the garden.`);
     }
-
+    //If the plant is not ripe, throw an Error:
     if (plant.ripe == false) {
       throw new Error(
         `The ${plantName} cannot be harvested before it is ripe.`
       );
     }
 
+    //Otherwise, this function should remove the plant from the 
+    //plants array, add it to storage with properties plantName and 
+    //quantity, free up the total space that the plant required, and return:
+    // "The {plantName} has been successfully harvested."
+
     this.plants = this.plants.filter((x) => x.plantName != plantName);
-    this.storage.push({ plantName, quantity: plant.quantity });
+    this.storage.push({
+      plantName,
+      quantity: plant.quantity
+    });
     this.spaceAvailable += plant.spaceRequired;
     return `The ${plantName} has been successfully harvested.`;
   }
 
   generateReport() {
+    //• On the first line:
+    //"The garden has { spaceAvailable } free space left."
     let toReturn = `The garden has ${this.spaceAvailable} free space left.\n`;
+
+    //On the second line list all plants that are in the garden ordered alphabetically by plant name ascending in the format:
+    //"Plants in the garden: {plant1Name}, {plant2Name}, {…}"
     toReturn += `Plants in the garden: ${this.plants
       .sort((a, b) => a.plantName.localeCompare(b.plantName))
       .map((x) => x.plantName)
       .join(", ")}\n`;
 
+    //On the third line add:
+    //If there are no plants in the storage, print:
+    //"Plants in storage: The storage is empty."
+    //If there are plants in the storage list them in the format:
+    //"Plants in storage: {plant1Name} ({plant1Quantity}), {plant2Name}, ({plant2Quantity}), {…}"
     toReturn += `Plants in storage: `;
     if (this.storage.length === 0) {
       toReturn += "The storage is empty.";
@@ -77,5 +100,21 @@ class Garden {
       toReturn += stringsToAdd.join(", ");
     }
     return toReturn;
+
+    // 2nd. way
+    const plantsAsString = this.plants.map(plant => plant.plantName).sort((a, b) => a.localeCompare(b));
+    const plantRow = `Plants in the garden: ${plantsAsString.join(', ')}`;
+    let storageRow = `Plants in storage: The storage is empty.`;
+    if (this.storage.length > 0) {
+      const storateAsString = this.storage.map(plant => `${plant.plantName} (${plant.quantity})`);
+      storageRow = `Plants in storage: ${storateAsString.join(', ')}`;
+    }
+    return [
+      `The garden has ${this.spaceAvailable} free space left.`,
+      plantRow,
+      storageRow,
+    ].join('\n');
+
   }
+
 }
